6aff7ca82719ee29f8ce089d0eaf5912d7783241
ospberd <ospberd@gmail.com>
2021-08-31 15:00:12 +0300

    1500
    15:00

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 let p =  document.getElementById("p2");
 let inp = document.getElementById("filtre");
 let fruits = ["Яблоко", "Апельсин", "Слива", "Малина"];
-fruits = [...fruits, "Вишенька"];
-fruits = [ "Ананас",...fruits];
 
-oninp ();
+arrList(fruits, p);
 
 function oninp() {
-p.innerHTML = '';
+fruits = addElem(fruits, inp)
+arrList(fruits, p);
+};
 
-if (inp.value != '') {
-    fruits = [ inp.value ,...fruits]
-    inp.value = ''};
+function onfiltr() {
+   let flist = filterItems(fruits, inp);
+   arrList(flist, p);
 
-fruits.forEach((item, i) => {
-    p.innerHTML += `<li>  ${item}  `
-});
+};
 
+function arrList(arr, paragr) {
+  p.innerHTML = '';
+  arr.forEach((item, i) => {
+      paragr.innerHTML += `<li>  ${item}  `
+  });
+};
+
+function addElem(arr, inp1){
+  if (inp1.value != '') {
+      arr = [ inp1.value ,...arr]
+      inp1.value = ''};
+  return arr;
+};
+
+function filterItems(arr, inp1) {
+  return arr.filter(function(el) {
+      return el.toLowerCase().indexOf(inp1.value.toLowerCase()) > -1;
+  })
 }
